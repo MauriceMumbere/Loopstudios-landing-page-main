af2355706a89a9cadd5865b944fdb192fe494d23
maurice Mumbere <mauricendayango@gmail.com>
2025-06-12 17:28:01 +0100

    project set-up

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup>
+useHead({
+  title: "Frontend Mentor | Loopstudios landing page",
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/images/favicon-32x32.png",
+    },
+  ],
+});
+</script>
+<template>
+  <div>
+    <h1>hello</h1>
+  </div>
+</template>
