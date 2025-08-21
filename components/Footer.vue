e8dd0bef30716823c6ad526ea66f0abc26891199
maurice Mumbere <mauricendayango@gmail.com>
2025-08-21 12:16:17 +0100

    feat: add Footer component with social media links and copyright notice

---
+++ components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer class="bg-black text-white px-5 md:px-[10rem] py-10">
+    <div
+      class="flex flex-col md:flex-row md:justify-between items-center text-center md:text-left"
+    >
+      <div>
+        <div>
+          <img src="../public/images/logo.svg" alt="Logo" />
+        </div>
+        <div class="md:pt-5">
+          <ul
+            class="flex flex-col items-center md:flex-row md:space-x-5 md:space-y-0 space-y-3 py-5"
+          >
+            <li><a href="#">About</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Support</a></li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div class="flex justify-center md:justify-end">
+          <ul class="flex items-center space-x-5">
+            <li>
+              <a
+                href="#"
+                class="relative inline-block before:content-[''] before:absolute before:top-8 before:-bottom-1 before:left-0 before:h-[2px] before:w-full before:bg-white before:scale-x-0 before:origin-center before:transition-transform before:duration-300 hover:before:scale-x-100"
+              >
+                <img src="/images/icon-facebook.svg" alt="Facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="relative inline-block before:content-[''] before:absolute before:top-8 before:-bottom-1 before:left-0 before:h-[2px] before:w-full before:bg-white before:scale-x-0 before:origin-center before:transition-transform before:duration-300 hover:before:scale-x-100"
+              >
+                <img src="/images/icon-twitter.svg" alt="Twitter" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="relative inline-block before:content-[''] before:absolute before:top-8 before:-bottom-1 before:left-0 before:h-[2px] before:w-full before:bg-white before:scale-x-0 before:origin-center before:transition-transform before:duration-300 hover:before:scale-x-100"
+              >
+                <img src="/images/icon-pinterest.svg" alt="Pinterest" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="relative inline-block before:content-[''] before:absolute before:top-8 before:-bottom-1 before:left-0 before:h-[2px] before:w-full before:bg-white before:scale-x-0 before:origin-center before:transition-transform before:duration-300 hover:before:scale-x-100"
+              >
+                <img src="/images/icon-instagram.svg" alt="Instagram" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p class="pt-5">© 2021 Loopstudios. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+</template>
