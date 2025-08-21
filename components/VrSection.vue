4c9d250e7f675ebb20a6775013cb84ea6eeae99a
maurice Mumbere <mauricendayango@gmail.com>
2025-08-21 12:15:44 +0100

    feat: add VrSection component for interactive VR display

---
+++ components/VrSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col md:flex-row px-5 md:px-[10rem] py-[2rem] relative">
+    <div>
+      <img src="../public/images/desktop/image-interactive.jpg" alt="" />
+    </div>
+    <div
+      class="bg-white md:p-20 text-center md:text-left md:absolute top-[15rem] right-[9rem] mt-10"
+    >
+      <div>
+        <h2 class="uppercase text-4xl md:text-5xl max-w-[30rem]">
+          The leader in interactive vr
+        </h2>
+      </div>
+      <div>
+        <p class="max-w-[30rem] mt-7">
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
